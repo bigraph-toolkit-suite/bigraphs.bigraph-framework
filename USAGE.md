1390ef976ef35c5dddd1b019aec8881d768b5d6a
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2019-05-28 16:58:05 +0200

    renamed module matching to rewriting

--- USAGE.md
+++ USAGE.md
@@ -6,3 +6,4 @@ This document describes the usage of the API for developers.
 ## Create Elementary Bigraphs
 
 Note on equality: Object equality at the instance level is not to be compared with mathematical equality of bigraphs.
+This is also know as bigraph isomorphism problem.
---
+++ rewriting/src/main/java/de/tudresden/inf/st/bigraphs/matching/Match.java
@@ -0,0 +1,35 @@
+package de.tudresden.inf.st.bigraphs.matching;
+
+import de.tudresden.inf.st.bigraphs.core.Bigraph;
+
+/**
+ * A match result for a ground bigraph
+ *
+ * @param <B>
+ */
+public interface Match<B extends Bigraph> {
+
+    B getContext();
+
+    /**
+     * Returns the redex of the reaction rule.
+     *
+     * @return the redex of the reaction rule
+     */
+    B getRedex();
+
+    /**
+     * juxtaposition of the redex and a suitable identity is called the redex image
+     *
+     * @return
+     */
+    B getRedexImage();
+
+    /**
+     * identity for the redex image
+     *
+     * @return
+     */
+    B getRedexIdentity();
+
+}
