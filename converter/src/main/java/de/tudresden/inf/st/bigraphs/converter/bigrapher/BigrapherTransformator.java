b19a692f0deaec4ded221dcffdd23130185efb54
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2019-11-09 15:31:16 +0100

    - bigrapher export - dependency added: apache commons lang 3

---
+++ converter/src/main/java/de/tudresden/inf/st/bigraphs/converter/bigrapher/BigrapherTransformator.java
@@ -0,0 +1,231 @@
+package de.tudresden.inf.st.bigraphs.converter.bigrapher;
+
+import de.tudresden.inf.st.bigraphs.converter.PrettyPrinter;
+import de.tudresden.inf.st.bigraphs.core.BigraphEntityType;
+import de.tudresden.inf.st.bigraphs.core.Control;
+import de.tudresden.inf.st.bigraphs.core.ControlKind;
+import de.tudresden.inf.st.bigraphs.core.Signature;
+import de.tudresden.inf.st.bigraphs.core.impl.BigraphEntity;
+import de.tudresden.inf.st.bigraphs.core.impl.pure.PureBigraph;
+import de.tudresden.inf.st.bigraphs.rewriting.ReactionRule;
+import de.tudresden.inf.st.bigraphs.rewriting.reactivesystem.impl.SimpleReactiveSystem;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Implementation of {@link PrettyPrinter} for the transformation of pure EMOF-based bigraphs to
+ * bigraphER's specification language. The whole bigraphical reactive system is encoded.
+ * Only one agent is printed.
+ * <p>
+ * Code is mostly adapted from the library jLibBig to our EMOF-based bigraph structure.
+ *
+ * @author Dominik Grzelak
+ */
+public class BigrapherTransformator implements PrettyPrinter<SimpleReactiveSystem> {
+
+    public static final String LINE_SEP = System.getProperty("line.separator");
+
+    private List<String> reactionLabels = new ArrayList<>();
+    private List<String> randomLinkNames = new ArrayList<>();
+    private int reactionRuleCounter = 0;
+
+    public BigrapherTransformator() {
+    }
+
+    @Override
+    public String toString(SimpleReactiveSystem system) {
+        StringBuilder s = new StringBuilder();
+
+        s.append(toString(system.getSignature()));
+
+        s.append(LINE_SEP);
+
+        s.append(toString(system.getReactionRules()));
+
+        s.append(LINE_SEP);
+
+        s.append(toStringForAgent(system.getAgent())).append(" ;").append(LINE_SEP);
+
+        s.append(LINE_SEP);
+        s.append("begin brs").append(LINE_SEP);
+        s.append("\tinit ").append(getAgentVarName(system.getAgent())).append(";").append(LINE_SEP);
+        String rrSet = String.join(",", reactionLabels).toString();
+        s.append("\trules = [{").append(rrSet).append("}];").append(LINE_SEP);
+        s.append("end");
+        s.append(LINE_SEP);
+
+        return s.toString();
+    }
+
+    private String toString(Collection<ReactionRule<PureBigraph>> reactionRules) {
+        StringBuilder s = new StringBuilder();
+        for (ReactionRule<PureBigraph> reaction : reactionRules) {
+            s.append(toString(reaction)).append(";").append(LINE_SEP);
+        }
+        return s.toString();
+    }
+
+    private String toString(ReactionRule<PureBigraph> reaction) {
+        StringBuilder sb = new StringBuilder("react ");
+        String rrName = reaction.getClass().getSimpleName().toLowerCase() + (reactionRuleCounter++);
+        reactionLabels.add(rrName);
+        sb.append(rrName).append(" = ");
+        String redex = toString(reaction.getRedex());
+        redex = !randomLinkNames.isEmpty() ? closeFakeLinks(randomLinkNames) + " (" + redex + ")" : redex;
+        randomLinkNames.clear();
+        String reactum = toString(reaction.getReactum());
+        reactum = !randomLinkNames.isEmpty() ? closeFakeLinks(randomLinkNames) + " (" + reactum + ")" : reactum;
+        randomLinkNames.clear();
+        sb.append(redex).append(" -> ").append(reactum);
+        return sb.toString();
+    }
+
+    private String toString(PureBigraph bigraph) {
+        StringBuilder s = new StringBuilder();
+        Iterator<BigraphEntity.RootEntity> iterator = bigraph.getRoots().iterator();
+        while (iterator.hasNext()) {
+            s.append('(');
+            BigraphEntity.RootEntity next = iterator.next();
+            Collection<BigraphEntity> childrenOf = bigraph.getChildrenOf(next);
+            if (!childrenOf.isEmpty()) {
+                Iterator<BigraphEntity> childIterator = childrenOf.iterator();
+                while (childIterator.hasNext()) {
+                    List<BigraphEntity.Link> links = new ArrayList<>(bigraph.getEdges());
+                    links.addAll(bigraph.getOuterNames());
+                    String s1 = toString(bigraph, childIterator.next(), links, bigraph.getSites());
+                    s.append(s1).append(childIterator.hasNext() ? " | " : "");
+                }
+            } else {
+//                s.append("nil");
+            }
+            s.append(iterator.hasNext() ? ") || " : ")");
+        }
+        return s.toString();
+    }
+
+    private String getAgentVarName(PureBigraph bigraph) {
+        return bigraph.getModelPackage().getName().toLowerCase();
+    }
+
+    private String toStringForAgent(PureBigraph bigraph) {
+        String varPrefix = "big " + getAgentVarName(bigraph) + " = ";
+        StringBuilder s = new StringBuilder("");
+        Iterator<BigraphEntity.RootEntity> iterator = bigraph.getRoots().iterator();
+        if (!iterator.hasNext()) return s.append(";").toString();
+        BigraphEntity.RootEntity next = iterator.next();
+        Collection<BigraphEntity> childrenOf = bigraph.getChildrenOf(next);
+        if (!childrenOf.isEmpty()) {
+            Iterator<BigraphEntity> childIterator = childrenOf.iterator();
+            while (childIterator.hasNext()) {
+                List<BigraphEntity.Link> links = new ArrayList<>(bigraph.getEdges());
+                links.addAll(bigraph.getOuterNames());
+                String s1 = toString(bigraph, childIterator.next(), links, bigraph.getSites());
+                s.append(s1).append(childIterator.hasNext() ? " | " : "");
+            }
+        } else {
+            s.append("1");
+        }
+
+        if (!randomLinkNames.isEmpty()) {
+            StringBuilder s2 = new StringBuilder();
+            String closeNames = closeFakeLinks(randomLinkNames);
+            s2.append(varPrefix).append(closeNames).append(" (").append(s.toString()).append(")");
+            randomLinkNames.clear();
+            return s2.toString();
+        } else {
+            return s.insert(0, varPrefix).toString();
+        }
+    }
+
+    private String toString(PureBigraph bigraph, BigraphEntity d, Collection<BigraphEntity.Link> collection, Collection<BigraphEntity.SiteEntity> sitelist) {
+        StringBuilder s = new StringBuilder();
+        if (BigraphEntityType.isSite(d)) {
+            s.append("id(1)"); //.append(((BigraphEntity.SiteEntity) d).getIndex());
+        } else {
+            s.append(d.getControl().getNamedType().stringValue());
+            StringBuilder ns = new StringBuilder();
+            int unlinked = 0;
+            int arity = d.getControl().getArity().getValue().intValue();
+            for (int a = 0; a < arity; a++) { //portIt.hasNext()) {
+                List<BigraphEntity.Port> portIt = new ArrayList<>(bigraph.getPorts(d));
+                BigraphEntity link = null;
+                boolean hasPort = (portIt.size() > 0 && a < portIt.size() && (portIt.get(a) != null) && (link = bigraph.getLinkOfPoint(portIt.get(a))) != null);
+                if (hasPort && (BigraphEntityType.isOuterName(link) || BigraphEntityType.isEdge(link)) && collection.contains(link)) {
+                    for (int i = 0; i < unlinked; ++i) {
+                        ns.append("-, ");
+                    }
+                    String name = ((BigraphEntity.Link) link).getName();
+                    ns.append(name).append(", ");
+                } else {
+                    ++unlinked;
+                }
+            }
+
+            if (ns.length() > 0) {
+                s.append("{").append(ns.substring(0, ns.length() - 2)).append("}");
+            }
+            // if no ports are connected but arity > 0: add fake name and close it later
+            if (ns.length() == 0 && unlinked != 0) {
+//                closeFakeNames = true;
+                String s1 = IntStream.range(0, arity)
+                        .mapToObj(x -> {
+                            String r = generateFakeLinkName();
+                            randomLinkNames.add(r);
+                            return r;
+                        })
+                        .collect(Collectors.joining(","));
+                s.append("{").append(s1).append("}");
+            }
+            Collection<BigraphEntity> children = bigraph.getChildrenOf(d);
+            if (!children.isEmpty()) {
+                s.append(".");
+                if (children.size() > 1) s.append("( ");
+
+                Iterator<BigraphEntity> childIt = children.iterator();
+                while (childIt.hasNext()) {
+                    s.append(toString(bigraph, childIt.next(), collection, sitelist))
+                            .append(childIt.hasNext() ? " | " : "");
+                }
+
+            } else {
+                if (d.getControl().getControlKind() != ControlKind.ATOMIC) // nesting for atomic controls not allowed
+                    s.append(".1");
+            }
+            if (children.size() > 1) s.append(" )");
+        }
+        return s.toString();
+    }
+
+    private String generateFakeLinkName() {
+        return RandomStringUtils.randomAlphabetic(4).toLowerCase();
+    }
+
+    private String closeFakeLinks(List<String> linkNames) {
+        return linkNames.stream().collect(Collectors.joining(" /", "/", ""));
+    }
+
+    @Override
+    public void toOutputStream(SimpleReactiveSystem system, OutputStream outputStream) throws IOException {
+        String s = toString(system);
+        outputStream.write(s.getBytes(), 0, s.length());
+    }
+
+    public static String toString(Signature sig) {
+        StringBuilder s = new StringBuilder();
+        Set<Control<?, ?>> controls = sig.getControls();
+        for (Control ctrl : controls) {
+            s
+                    .append(ctrl.getControlKind().equals(ControlKind.ATOMIC) ? "" : "atomic ")
+                    .append("ctrl ")
+                    .append(ctrl.getNamedType().stringValue()).append(" = ")
+                    .append(ctrl.getArity().getValue()).append(";")
+                    .append(LINE_SEP);
+        }
+        return s.toString();
+    }
+}
---
+++ converter/src/test/java/de/tudresden/inf/st/bigraphs/converter/bigrapher/BigrapherTransformationUnitTest.java
@@ -0,0 +1,122 @@
+package de.tudresden.inf.st.bigraphs.converter.bigrapher;
+
+import de.tudresden.inf.st.bigraphs.core.Control;
+import de.tudresden.inf.st.bigraphs.core.Signature;
+import de.tudresden.inf.st.bigraphs.core.datatypes.FiniteOrdinal;
+import de.tudresden.inf.st.bigraphs.core.datatypes.StringTypedName;
+import de.tudresden.inf.st.bigraphs.core.exceptions.ControlIsAtomicException;
+import de.tudresden.inf.st.bigraphs.core.exceptions.InvalidConnectionException;
+import de.tudresden.inf.st.bigraphs.core.exceptions.InvalidReactionRuleException;
+import de.tudresden.inf.st.bigraphs.core.exceptions.builder.LinkTypeNotExistsException;
+import de.tudresden.inf.st.bigraphs.core.factory.AbstractBigraphFactory;
+import de.tudresden.inf.st.bigraphs.core.factory.PureBigraphFactory;
+import de.tudresden.inf.st.bigraphs.core.impl.BigraphEntity;
+import de.tudresden.inf.st.bigraphs.core.impl.DefaultDynamicControl;
+import de.tudresden.inf.st.bigraphs.core.impl.DefaultDynamicSignature;
+import de.tudresden.inf.st.bigraphs.core.impl.builder.DynamicSignatureBuilder;
+import de.tudresden.inf.st.bigraphs.core.impl.pure.PureBigraph;
+import de.tudresden.inf.st.bigraphs.core.impl.pure.PureBigraphBuilder;
+import de.tudresden.inf.st.bigraphs.rewriting.ReactionRule;
+import de.tudresden.inf.st.bigraphs.rewriting.reactivesystem.ParametricReactionRule;
+import de.tudresden.inf.st.bigraphs.rewriting.reactivesystem.impl.SimpleReactiveSystem;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+/**
+ * @author Dominik Grzelak
+ */
+public class BigrapherTransformationUnitTest {
+    private static PureBigraphFactory<StringTypedName, FiniteOrdinal<Integer>> factory = AbstractBigraphFactory.createPureBigraphFactory();
+
+    /**
+     * bigrapher full -d ./test -f svg -s states -M 10 -t trans.svg -v test.big
+     */
+    @Test
+    void name() throws InvalidConnectionException, LinkTypeNotExistsException, InvalidReactionRuleException, IOException {
+        SimpleReactiveSystem reactiveSystem = new SimpleReactiveSystem();
+
+        PureBigraph agent_a = createAgent_A();
+        ReactionRule<PureBigraph> rr_1 = createReactionRule_1();
+
+        reactiveSystem.setAgent(agent_a);
+        reactiveSystem.addReactionRule(rr_1);
+
+        BigrapherTransformator prettyPrinter = new BigrapherTransformator();
+        String s = prettyPrinter.toString(reactiveSystem);
+        System.out.println(s);
+//
+//        FileOutputStream fout = new FileOutputStream(new File("/home/dominik/git/BigraphFramework/converter/src/test/resources/dump/couting.bgm"));
+//        prettyPrinter.toOutputStream(reactiveSystem, fout);
+//        fout.close();
+    }
+
+    public static PureBigraph createAgent_A() throws ControlIsAtomicException, InvalidConnectionException, LinkTypeNotExistsException {
+        Signature<DefaultDynamicControl<StringTypedName, FiniteOrdinal<Integer>>> signature = createExampleSignature();
+        PureBigraphBuilder<DefaultDynamicSignature> builder = factory.createBigraphBuilder(signature);
+        BigraphEntity.OuterName jeff1 = builder.createOuterName("jeff1");
+        BigraphEntity.OuterName jeff2 = builder.createOuterName("jeff2");
+        BigraphEntity.OuterName b1 = builder.createOuterName("b1");
+        BigraphEntity.InnerName e1 = builder.createInnerName("e1");
+
+        builder.createRoot()
+//                .addChild("Printer")
+                .addChild(signature.getControlByName("Room")).connectNodeToInnerName(e1)
+                .withNewHierarchy()
+                .addChild(signature.getControlByName("Computer")).connectNodeToOuterName(b1)
+                .withNewHierarchy().addChild(signature.getControlByName("Job")).goBack()
+                .addChild(signature.getControlByName("User")).connectNodeToOuterName(jeff1)
+                .goBack()
+
+                .addChild(signature.getControlByName("Room")).connectNodeToInnerName(e1)
+                .withNewHierarchy()
+                .addChild(signature.getControlByName("Computer")).connectNodeToOuterName(b1)
+                .withNewHierarchy().addChild(signature.getControlByName("Job")).addChild(signature.getControlByName("User")).connectNodeToOuterName(jeff2)
+                .goBack().goBack();
+
+        builder.closeAllInnerNames();
+        builder.makeGround();
+        return builder.createBigraph();
+    }
+
+    public static ReactionRule<PureBigraph> createReactionRule_1() throws LinkTypeNotExistsException, InvalidConnectionException, ControlIsAtomicException, InvalidReactionRuleException {
+        Signature<DefaultDynamicControl<StringTypedName, FiniteOrdinal<Integer>>> signature = createExampleSignature();
+        PureBigraphBuilder<DefaultDynamicSignature> builder = factory.createBigraphBuilder(signature);
+        BigraphEntity.OuterName jeff1 = builder.createOuterName("jeff1");
+        BigraphEntity.OuterName jeff2 = builder.createOuterName("jeff2");
+        BigraphEntity.OuterName b1 = builder.createOuterName("b1");
+
+        //(Computer{b1}.(Job.1) | User{jeff2}.1) || Computer{b1}.(Job.1 | User{jeff2}.1);
+
+        builder.createRoot()
+//                .addChild("Printer")
+                .addChild(signature.getControlByName("Computer")).connectNodeToOuterName(b1)
+                .withNewHierarchy().addChild(signature.getControlByName("Job")).goBack()
+                .addChild(signature.getControlByName("User")).connectNodeToOuterName(jeff1);
+
+        builder.createRoot()
+                .addChild(signature.getControlByName("Computer")).connectNodeToOuterName(b1)
+                .withNewHierarchy().addChild(signature.getControlByName("Job")).addChild(signature.getControlByName("User")).connectNodeToOuterName(jeff2);
+
+//        builder.makeGround();
+        PureBigraph redex = builder.createBigraph();
+        ReactionRule<PureBigraph> rr = new ParametricReactionRule<>(redex, redex);
+        return rr;
+    }
+
+    private static <C extends Control<?, ?>, S extends Signature<C>> S createExampleSignature() {
+        DynamicSignatureBuilder<StringTypedName, FiniteOrdinal<Integer>> defaultBuilder = factory.createSignatureBuilder();
+        defaultBuilder
+                .newControl().identifier(StringTypedName.of("Printer")).arity(FiniteOrdinal.ofInteger(2)).assign()
+                .newControl().identifier(StringTypedName.of("Building")).arity(FiniteOrdinal.ofInteger(0)).assign()
+                .newControl().identifier(StringTypedName.of("User")).arity(FiniteOrdinal.ofInteger(1)).assign()
+                .newControl().identifier(StringTypedName.of("Room")).arity(FiniteOrdinal.ofInteger(1)).assign()
+                .newControl().identifier(StringTypedName.of("Spool")).arity(FiniteOrdinal.ofInteger(1)).assign()
+                .newControl().identifier(StringTypedName.of("Computer")).arity(FiniteOrdinal.ofInteger(1)).assign()
+                .newControl().identifier(StringTypedName.of("Job")).arity(FiniteOrdinal.ofInteger(0)).assign()
+                .newControl().identifier(StringTypedName.of("A")).arity(FiniteOrdinal.ofInteger(1)).assign()
+                .newControl().identifier(StringTypedName.of("B")).arity(FiniteOrdinal.ofInteger(1)).assign();
+
+        return (S) defaultBuilder.create();
+    }
+}
