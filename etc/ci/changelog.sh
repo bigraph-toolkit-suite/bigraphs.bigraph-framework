47d701c7ddaad8ed155613f1001ab2e857c383d6
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2019-12-02 15:19:09 +0100

    changelog generation script improved

--- etc/ci/changelog.sh
+++ etc/ci/changelog.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "Creating changelog ..."
 
+baseuriGit=https://git-st.inf.tu-dresden.de/bigraphs/bigraph-framework/commit/
+
+getGitLogsFor() {
+  formatOutput="- %s [view commit &#x2197;](${baseuriGit}%H)"
+  local result=$(git log --oneline --pretty=format:"${formatOutput}" --reverse | grep $1)
+  replaceString="- "
+  result=$(echo "$result" | sed "s/- - /$replaceString/")
+  echo "${result}"
+  return 0
+}
+
 if [ "$1" != "" ]; then
   revisionFile=$1
 else
@@ -16,25 +27,28 @@ fi
 echo "[*] Version is: ${version}"
 
 if [ "$2" != "" ]; then
-  filename=$1/changelog_${version}.txt
+  filename=$1/changelog_${version}.md
 else
-  filename=./changelog_${version}.txt
+  filename=./changelog_${version}.md
 fi
+
 echo "[*] Output path set to=${filename}"
 echo "[*] Writing changelog now ..."
 
-echo "# Changelog - ${version} [$(date)]" >${filename}
+echo "## [${version}] - $(date '+%F')" >${filename}
 
-echo "## Added" >>${filename}
-git log --oneline --pretty=format:'<li> <a href="commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep ^added: >>${filename}
+#git log --oneline --pretty=format:'<li> <a href="commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep ^added: >>${filename}
 
-echo "## Changed" >>${filename}
-git log --oneline --pretty=format:'<li> <a href="commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep ^changed: >>${filename}
+echo "### Added" >>${filename}
+echo "$(getGitLogsFor 'added')" >> ${filename}
 
-echo "## Removed" >>${filename}
-git log --oneline --pretty=format:'<li> <a href="commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep ^removed: >>${filename}
+echo "### Changed" >>${filename}
+echo "$(getGitLogsFor 'changed')" >> ${filename}
 
-echo "## Bugfix" >>${filename}
-git log --oneline --pretty=format:'<li> <a href="commit/%H">view commit &bull;</a> %s</li> ' --reverse | grep ^bugfix: >>${filename}
+echo "### Removed" >>${filename}
+echo "$(getGitLogsFor 'removed')" >> ${filename}
+
+echo "### Bugfix" >>${filename}
+echo "$(getGitLogsFor 'bugfix')" >> ${filename}
 
 echo "[*] Writing changelog now ... finished"
