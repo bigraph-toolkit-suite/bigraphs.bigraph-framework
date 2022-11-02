f15f056e4b5a2ed8be64c169b33b8eabfed96aa9
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2022-11-02 15:35:17 +0100

    new ReactionGraphExporter for visualization module (move exporter from simu module here)

---
+++ visualization/src/main/java/de/tudresden/inf/st/bigraphs/visualization/ReactionGraphExporter.java
@@ -0,0 +1,14 @@
+package de.tudresden.inf.st.bigraphs.visualization;
+
+import de.tudresden.inf.st.bigraphs.core.reactivesystem.ReactionGraph;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReactionGraphExporter implements BigraphGraphicsExporter<ReactionGraph> {
+
+    @Override
+    public void toPNG(ReactionGraph reactionGraph, File file) throws IOException {
+
+    }
+}
