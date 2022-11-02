65a9a5996377a3160745c1ca46f9ee3f7eba9536
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2022-11-02 15:39:43 +0100

    make abstract class

--- core/src/main/java/de/tudresden/inf/st/bigraphs/core/exceptions/builder/TypeNotExistsException.java
+++ core/src/main/java/de/tudresden/inf/st/bigraphs/core/exceptions/builder/TypeNotExistsException.java
@@ -3,7 +3,7 @@ package de.tudresden.inf.st.bigraphs.core.exceptions.builder;
 /**
  * @author Dominik Grzelak
  */
-public class TypeNotExistsException extends Exception {
+public abstract class TypeNotExistsException extends Exception {
 
     public TypeNotExistsException() {
         this("This bigraph type doesn't exists.");
