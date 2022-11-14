e4aa7113a8f26e279fd48c3a581ad2079b51417c
Dominik Grzelak <dominik.grzelak@tu-dresden.de>
2022-11-14 11:06:23 +0100

    dependency-check-maven vuln. issue fixed

--- documentation/v2-docusaurus/docs/assets/converter/bigrapher_test01-0.java
+++ documentation/v2-docusaurus/docs/assets/converter/bigrapher_test01-0.java
@@ -5,5 +5,8 @@ public void bigrapher_test01() throws InvalidConnectionException, TypeNotExistsE
 	DefaultDynamicSignature sig = createSignature();
 	createOrGetBigraphMetaModel(sig);
 	PureBigraphBuilder<DefaultDynamicSignature> builder = pureBuilder(sig);
-	PureBigraph s0 = builder.createRoot().addChild("A", "a").down().addChild("Snd").down().addChild("M", "a").linkToOuter("v_a").addChild("Ready").down().addChild("Fun").top().addChild("A", "b").down().addChild("Snd").down().addChild("M", "a").linkToOuter("v_b").top().addChild("Mail").createBigraph();
+	PureBigraph s0 = builder.createRoot().// A{a}.Snd.(M{a, v_a} | Ready.Fun.1);
+addChild("A", "a").down().addChild("Snd").down().addChild("M", "a").linkToOuter("v_a").addChild("Ready").down().addChild("Fun").top().// A{b}.Snd.(M{a, v_b});
+addChild("A", "b").down().addChild("Snd").down().addChild("M", "a").linkToOuter("v_b").top().// Mail.1
+addChild("Mail").createBigraph();
 }
--- documentation/v2-docusaurus/docs/assets/converter/bigrapher_test01-1.java
+++ documentation/v2-docusaurus/docs/assets/converter/bigrapher_test01-1.java
@@ -1,10 +1,13 @@
 
 public void bigrapher_test01() throws InvalidConnectionException, TypeNotExistsException, InvalidReactionRuleException, IOException { 
 	// ...
-	PureBigraph sndRedex = builder.spawnNewOne().createRoot().addChild("A", "a0").down().addChild("Snd").down().addChild("M", "a1").linkToOuter("v").addSite().top().addChild("Mail").createBigraph();
-	PureBigraph sndReactum = builder.spawnNewOne().createRoot().addChild("A", "a0").addChild("Mail").down().addChild("M", "a1").linkToOuter("v").addSite().createBigraph();
+	PureBigraph sndRedex = builder.spawnNewOne().createRoot().// A{a0}.Snd.(M{a1, v} | id) | Mail
+addChild("A", "a0").down().addChild("Snd").down().addChild("M", "a1").linkToOuter("v").addSite().top().addChild("Mail").createBigraph();
+	PureBigraph sndReactum = builder.spawnNewOne().createRoot().// A{a0} | Mail.(M{a1, v} | id);
+addChild("A", "a0").addChild("Mail").down().addChild("M", "a1").linkToOuter("v").addSite().createBigraph();
 	ParametricReactionRule<PureBigraph> snd = new ParametricReactionRule<>(sndRedex, sndReactum).withLabel("snd");
-	PureBigraph readyRedex = builder.spawnNewOne().createRoot().addChild("A", "a").down().addChild("Ready").up().addChild("Mail").down().addChild("M", "a").linkToOuter("v").addSite().createBigraph();
+	PureBigraph readyRedex = builder.spawnNewOne().createRoot().// A{a}.Ready | Mail.(M{a, v} | id)
+addChild("A", "a").down().addChild("Ready").up().addChild("Mail").down().addChild("M", "a").linkToOuter("v").addSite().createBigraph();
 	PureBigraphBuilder<DefaultDynamicSignature> readyBuilder = builder.spawnNewOne();
 	readyBuilder.createOuterName("v");
 	PureBigraph readyReactum = readyBuilder.createRoot().addChild("A", "a").addChild("Mail").createBigraph();
@@ -12,8 +15,11 @@ public void bigrapher_test01() throws InvalidConnectionException, TypeNotExistsE
 	PureBigraph lambdaRedex = builder.spawnNewOne().createRoot().addChild("A", "a").down().addChild("Fun").createBigraph();
 	PureBigraph lambdaReactum = builder.spawnNewOne().createRoot().addChild("A", "a").createBigraph();
 	ParametricReactionRule<PureBigraph> lambda = new ParametricReactionRule<>(lambdaRedex, lambdaReactum).withLabel("lambda");
-	PureBigraph newRedex = builder.spawnNewOne().createRoot().addChild("A", "a0").down().addChild("New").down().addChild("A'", "a1").down().addSite().up().addSite().up().addSite().createBigraph();
-	PureBigraph newReactum = builder.spawnNewOne().createRoot().addChild("A", "a0").down().addSite().addSite().up().addChild("A", "a1").down().addSite().addSite().createBigraph();
+	PureBigraph newRedex = builder.spawnNewOne().createRoot().// A{a0}.(New.(A'{a1} | id) | id)
+addChild("A", "a0").down().addChild("New").down().addChild("A'", "a1").down().addSite().// (!) explicitly adding a site here, ohterwise BigraphER encoding will be not correct
+up().addSite().up().addSite().createBigraph();
+	PureBigraph newReactum = builder.spawnNewOne().createRoot().// A{a0}.(id | id) | A{a1}.(id | id)
+addChild("A", "a0").down().addSite().addSite().up().addChild("A", "a1").down().addSite().addSite().createBigraph();
 	InstantiationMap instMap = InstantiationMap.create(newReactum.getSites().size()).map(0, 1).map(1, 2).map(2, 0).map(3, 2);
 	ParametricReactionRule<PureBigraph> newRR = new ParametricReactionRule<>(newRedex, newReactum, instMap).withLabel("new");
 }
