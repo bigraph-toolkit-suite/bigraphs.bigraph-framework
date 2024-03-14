a67669548e1044de6d74d66a3c33ee19148eed5e
Dominik Grzelak <dominik@offbeat-pioneer.net>
2024-03-14 14:33:31 +0100

    Visualization of Compiler Graphs (VCG) transformation implemented (yComp reads VCG)

---
+++ converter/src/main/java/org/bigraphs/framework/converter/vcg/VCGTransformator.java
@@ -0,0 +1,212 @@
+package org.bigraphs.framework.converter.vcg;
+
+import com.google.common.graph.Traverser;
+import org.bigraphs.framework.converter.BigraphPrettyPrinter;
+import org.bigraphs.framework.core.BigraphEntityType;
+import org.bigraphs.framework.core.impl.BigraphEntity;
+import org.bigraphs.framework.core.impl.pure.PureBigraph;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static org.bigraphs.framework.core.BigraphMetaModelConstants.*;
+
+/**
+ * This pretty printer class transforms pure bigraphs of type {@link PureBigraph} to the  Visualization of Compiler Graphs (VCG) format.
+ * The tool yComp shipped with GrGen.NET can read and visualize this format.
+ *
+ * @author Dominik Grzelak
+ */
+public class VCGTransformator implements BigraphPrettyPrinter<PureBigraph> {
+    protected String LINE_SEP = System.getProperty("line.separator");
+
+    @Override
+    public String toString(PureBigraph bigraph) {
+        return encode(bigraph);
+    }
+
+    @Override
+    public void toOutputStream(PureBigraph bigraph, OutputStream outputStream) throws IOException {
+        String converted = encode(bigraph);
+        byte[] bytes = converted.getBytes();
+        outputStream.write(bytes);
+        outputStream.flush(); // Flush the OutputStream to ensure all data is written
+    }
+
+    // node&edge declarations
+    String vcgTemplate_Body = "graph:{\n" +
+            " infoname 1: \"Attributes\"\n" +
+            " display_edge_labels: no\n" +
+            " layoutalgorithm: normal //$ \"Compilergraph\"\n" +
+            " port_sharing: no\n" +
+            " splines: no\n" +
+            " manhattan_edges: no\n" +
+            " smanhattan_edges: no\n" +
+            " orientation: bottom_to_top\n" +
+            " edges: yes\n" +
+            " nodes: yes\n" +
+            "%s\n" +
+            "}\n";
+    // id, label
+    String vcgTemplate_Node = " node:{title:\"%s\" label:\"%s\" info1: \"\" color:yellow bordercolor:darkyellow}";
+    String vcgTemplate_Site = " node:{title:\"%s\" label:\"%s\" info1: \"\" color:white bordercolor:darkyellow}";
+    // sourceId, targetId, label
+    String vcgTemplate_Edge = " edge:{sourcename:\"%s\" targetname:\"%s\" label:\"%s\" color:darkyellow}";
+
+    Supplier<String> createNameSupplier(final String prefix) {
+        return new Supplier<>() {
+            private int id = 0;
+
+            @Override
+            public String get() {
+                return prefix + id++;
+            }
+        };
+    }
+
+    private String encode(PureBigraph bigraph) {
+        StringBuilder sb = new StringBuilder();
+
+        // (1) Loop through the node hierarchy using a BFS
+        // Create all root, site, node and port elements of the graph first.
+        // Also establish parent-child relations
+        // Store in a map: edge/outer -> port of node (for the second run)
+        Map<BigraphEntity.Link, LinkedHashSet<BigraphEntity.Port>> linkPortMap = new HashMap<>();
+        Map<BigraphEntity.Link, String> linkNameMap = new HashMap<>();
+        Map<BigraphEntity.Port, String> portNameMap = new HashMap<>();
+        Traverser<BigraphEntity> traverser = Traverser.forTree(x -> {
+            List<BigraphEntity<?>> children = bigraph.getChildrenOf(x);
+            return children;
+        });
+        Iterable<BigraphEntity> bigraphEntities = traverser.breadthFirst(bigraph.getRoots());
+
+        // Place Graph
+        // All nodes, roots and sites are created
+        // ports are linked to nodes
+        bigraphEntities.forEach(x -> {
+            if (BigraphEntityType.isRoot(x)) {
+                String rootVar = "r" + ((BigraphEntity.RootEntity) x).getIndex();
+                sb.append(String.format(vcgTemplate_Node, rootVar, rootVar + ":" + CLASS_ROOT));
+                sb.append(LINE_SEP);
+            }
+            if (BigraphEntityType.isSite(x)) {
+                String siteVar = "s" + ((BigraphEntity.SiteEntity) x).getIndex();
+                sb.append(String.format(vcgTemplate_Site, siteVar, siteVar + ":" + CLASS_SITE));
+                sb.append(LINE_SEP);
+            }
+            if (BigraphEntityType.isNode(x)) {
+                BigraphEntity.NodeEntity nodeEntity = (BigraphEntity.NodeEntity) x;
+                sb.append(String.format(vcgTemplate_Node, nodeEntity.getName(), nodeEntity.getName() + ":" + nodeEntity.getControl().getNamedType().stringValue()));
+                sb.append(LINE_SEP);
+
+                bigraph.getPorts(nodeEntity)
+                        .forEach(p -> {
+                            BigraphEntity.Link link = bigraph.getLinkOfPoint(p);
+                            if (link != null) {
+                                if (!linkPortMap.containsKey(link)) {
+                                    linkPortMap.put(link, new LinkedHashSet<>());
+                                }
+                                linkPortMap.get(link).add(p);
+                            }
+                        });
+
+                // Create as many Port nodes wrt the control's arity
+                for (int i = 0; i < nodeEntity.getControl().getArity().getValue().intValue(); i++) {
+                    String portVar = nodeEntity.getName() + "_" + "p" + i;
+                    portNameMap.put(bigraph.getPorts(nodeEntity).get(i), portVar);
+                    sb.append(String.format(vcgTemplate_Node, portVar, CLASS_PORT));
+                    sb.append(LINE_SEP);
+                    sb.append(
+                            String.format(vcgTemplate_Edge, portVar, nodeEntity.getName(), REFERENCE_NODE)
+                    );
+                    sb.append(LINE_SEP);
+                    sb.append(LINE_SEP);
+                }
+            }
+        });
+        // Rerun BFS again to create the parent-child relations now that we know we can access the node variables
+        Iterable<BigraphEntity> bigraphEntities2 = traverser.breadthFirst(bigraph.getRoots());
+        bigraphEntities2.forEach(x -> {
+            String nodeVar = "";
+            if (BigraphEntityType.isRoot(x)) {
+                nodeVar = "r" + ((BigraphEntity.RootEntity) x).getIndex();
+            }
+            if (BigraphEntityType.isNode(x)) {
+                BigraphEntity.NodeEntity nodeEntity = (BigraphEntity.NodeEntity) x;
+                nodeVar = nodeEntity.getName();
+            }
+            // Create opposite edges bPrnt,bChilds
+            for (BigraphEntity<?> each : bigraph.getChildrenOf(x)) {
+                if (BigraphEntityType.isNode(each)) {
+                    sb.append(
+                            String.format(vcgTemplate_Edge, ((BigraphEntity.NodeEntity) each).getName(), nodeVar, REFERENCE_PARENT)
+                    );
+                    sb.append(LINE_SEP);
+                }
+                if (BigraphEntityType.isSite(each)) {
+                    String siteVar = "s" + ((BigraphEntity.SiteEntity) each).getIndex();
+                    sb.append(
+                            String.format(vcgTemplate_Edge, siteVar, nodeVar, REFERENCE_PARENT)
+                    );
+                    sb.append(LINE_SEP);
+
+                }
+            }
+        });
+
+        // Link Graph
+        Set<Map.Entry<BigraphEntity.Link, LinkedHashSet<BigraphEntity.Port>>> entries = linkPortMap.entrySet();
+        for (Map.Entry<BigraphEntity.Link, LinkedHashSet<BigraphEntity.Port>> each : entries) {
+            BigraphEntity.Link linkElem = each.getKey();
+            Set<BigraphEntity.Port> ports = each.getValue();
+
+            String nodeType = BigraphEntityType.isEdge(linkElem) ? CLASS_EDGE : CLASS_OUTERNAME;
+            String nodeVar = linkElem.getName();
+
+            linkNameMap.put(linkElem, nodeVar);
+            sb.append(
+                            String.format(vcgTemplate_Node, nodeVar, nodeVar + ":" + nodeType)
+                    )
+                    .append(LINE_SEP);
+            // Create opposite edges bLink,bPoints
+            for (BigraphEntity.Port p : ports) {
+                String portVar = portNameMap.get(p);
+                sb.append(
+                        String.format(vcgTemplate_Edge, portVar, nodeVar, REFERENCE_LINK)
+                );
+                sb.append(LINE_SEP);
+            }
+        }
+        // Idle outer names
+        List<BigraphEntity.OuterName> ol = new ArrayList<>(bigraph.getOuterNames());
+        ol.removeAll(entries.stream().map(x -> x.getKey()).collect(Collectors.toList()));
+        for (BigraphEntity.OuterName each : ol) {
+            sb.append(
+                            String.format(vcgTemplate_Node, each.getName(), each.getName() + ":" + CLASS_OUTERNAME)
+                    )
+                    .append(LINE_SEP);
+        }
+
+        // All inner names
+        List<BigraphEntity.InnerName> il = new ArrayList<>(bigraph.getInnerNames());
+        for (BigraphEntity.InnerName each : il) {
+            sb.append(
+                            String.format(vcgTemplate_Node, each.getName(), each.getName() + ":" + CLASS_INNERNAME)
+                    )
+                    .append(LINE_SEP);
+            BigraphEntity.Link linkOfPoint = bigraph.getLinkOfPoint(each);
+            if (linkOfPoint != null) {
+                sb.append(
+                        String.format(vcgTemplate_Edge, each.getName(), linkOfPoint.getName(), REFERENCE_LINK)
+                );
+            }
+            sb.append(LINE_SEP);
+        }
+
+        String result = String.format(vcgTemplate_Body, sb);
+        return result;
+    }
+}
---
+++ converter/src/test/java/org/bigraphs/framework/converter/vcg/VcgTransformationUnitTest.java
@@ -0,0 +1,103 @@
+package org.bigraphs.framework.converter.vcg;
+
+import org.bigraphs.framework.core.BigraphFileModelManagement;
+import org.bigraphs.framework.core.Control;
+import org.bigraphs.framework.core.Signature;
+import org.bigraphs.framework.core.exceptions.ControlIsAtomicException;
+import org.bigraphs.framework.core.exceptions.InvalidConnectionException;
+import org.bigraphs.framework.core.exceptions.InvalidReactionRuleException;
+import org.bigraphs.framework.core.exceptions.builder.TypeNotExistsException;
+import org.bigraphs.framework.core.impl.BigraphEntity;
+import org.bigraphs.framework.core.impl.pure.PureBigraph;
+import org.bigraphs.framework.core.impl.pure.PureBigraphBuilder;
+import org.bigraphs.framework.core.impl.signature.DefaultDynamicSignature;
+import org.bigraphs.framework.core.impl.signature.DynamicSignatureBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static org.bigraphs.framework.core.factory.BigraphFactory.pureBuilder;
+import static org.bigraphs.framework.core.factory.BigraphFactory.pureSignatureBuilder;
+
+public class VcgTransformationUnitTest {
+    private static final String DUMP_TARGET = "src/test/resources/dump/";
+
+    /**
+     * ycomp ycomp-test.vcg
+     */
+    @Test
+    void name() throws InvalidConnectionException, TypeNotExistsException, InvalidReactionRuleException, IOException {
+        PureBigraph big = createBigraph();
+//        BigraphFileModelManagement.Store.exportAsInstanceModel(big, System.out);
+
+        VCGTransformator vcgTransformator = new VCGTransformator();
+        String s = vcgTransformator.toString(big);
+        System.out.println(s);
+
+        FileOutputStream fout = new FileOutputStream(new File(DUMP_TARGET + "ycomp-test.vcg"));
+        vcgTransformator.toOutputStream(big, fout);
+        fout.close();
+    }
+
+    public PureBigraph createBigraph() throws ControlIsAtomicException, InvalidConnectionException, TypeNotExistsException {
+        DefaultDynamicSignature signature = createExampleSignature();
+        PureBigraphBuilder<DefaultDynamicSignature> builder = pureBuilder(signature);
+        BigraphEntity.OuterName idleOuter = builder.createOuterName("idle2");
+        BigraphEntity.OuterName jeff1 = builder.createOuterName("jeff1");
+        BigraphEntity.OuterName jeff2 = builder.createOuterName("jeff2");
+        BigraphEntity.OuterName b1 = builder.createOuterName("b1");
+        BigraphEntity.OuterName a = builder.createOuterName("a");
+        BigraphEntity.OuterName b = builder.createOuterName("b");
+        BigraphEntity.InnerName e1 = builder.createInnerName("e1");
+        BigraphEntity.InnerName idleInner = builder.createInnerName("idle1");
+
+
+        builder.createRoot()
+                .addChild("Printer").linkToOuter(a).linkToOuter(b)
+                .addChild(signature.getControlByName("Room")).linkToInner(e1)
+                .down()
+                .addSite()
+                .addChild(signature.getControlByName("Computer")).linkToOuter(b1)
+                .down().addChild(signature.getControlByName("Job")).up()
+                .addChild(signature.getControlByName("User")).linkToOuter(jeff1)
+                .up()
+
+                .addChild(signature.getControlByName("Room")).linkToInner(e1)
+                .down()
+                .addSite()
+                .addChild(signature.getControlByName("Computer")).linkToOuter(b1)
+                .down().addChild(signature.getControlByName("Job")).addChild(signature.getControlByName("User")).linkToOuter(jeff2)
+                .up().up();
+
+//        builder.closeAllInnerNames();
+//        builder.makeGround();
+        return builder.createBigraph();
+    }
+
+    private static <C extends Control<?, ?>, S extends Signature<C>> S createExampleSignature() {
+        DynamicSignatureBuilder defaultBuilder = pureSignatureBuilder();
+        defaultBuilder
+                .addControl("Printer", 2)
+                .addControl("Building", 0)
+                .addControl("User", 1)
+                .addControl("Room", 1)
+                .addControl("Spool", 1)
+                .addControl("Computer", 1)
+                .addControl("Job", 0)
+                .addControl("A", 1)
+                .addControl("B", 1)
+//                .newControl().identifier(StringTypedName.of("Printer")).arity(FiniteOrdinal.ofInteger(2)).assign()
+//                .newControl().identifier(StringTypedName.of("Building")).arity(FiniteOrdinal.ofInteger(0)).assign()
+//                .newControl().identifier(StringTypedName.of("User")).arity(FiniteOrdinal.ofInteger(1)).assign()
+//                .newControl().identifier(StringTypedName.of("Room")).arity(FiniteOrdinal.ofInteger(1)).assign()
+//                .newControl().identifier(StringTypedName.of("Spool")).arity(FiniteOrdinal.ofInteger(1)).assign()
+//                .newControl().identifier(StringTypedName.of("Computer")).arity(FiniteOrdinal.ofInteger(1)).assign()
+//                .newControl().identifier(StringTypedName.of("Job")).arity(FiniteOrdinal.ofInteger(0)).assign()
+//                .newControl().identifier(StringTypedName.of("A")).arity(FiniteOrdinal.ofInteger(1)).assign()
+//                .newControl().identifier(StringTypedName.of("B")).arity(FiniteOrdinal.ofInteger(1)).assign();
+        ;
+        return (S) defaultBuilder.create();
+    }
+}
